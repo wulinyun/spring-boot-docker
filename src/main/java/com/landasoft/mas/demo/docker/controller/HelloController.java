9c94674b940577909380b0252b7c5342cac677d1
wulinyun <lin.wu@landasoft.com>
2018-08-28 20:51:14 +0800

    更新测试阿里云镜像不覆盖问题是否存在

--- src/main/java/com/landasoft/mas/demo/docker/controller/HelloController.java
+++ src/main/java/com/landasoft/mas/demo/docker/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
     public String home() {
-        return "Hello Docker World update";
+        return "Hello Docker World";
     }
 }
