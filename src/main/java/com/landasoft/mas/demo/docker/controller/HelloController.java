2c22c6acfa6549d9cb1804ac99a812136d5cb35d
wulinyun <lin.wu@landasoft.com>
2018-08-28 22:09:51 +0800

    更新

--- src/main/java/com/landasoft/mas/demo/docker/controller/HelloController.java
+++ src/main/java/com/landasoft/mas/demo/docker/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
     public String home() {
-        return "Hello Docker World Update";
+        return "Hello Docker World Update！！！！！！";
     }
 }
